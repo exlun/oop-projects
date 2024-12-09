2ab912ae3816c9daa38c312e6972ac9b7e039f77
exlun <144317988+exlun@users.noreply.github.com>
2024-12-09 14:47:47 +0300

    feat: add basic data units

---
+++ src/Lab4/DataUnits/Directories/Directory.cs
@@ -0,0 +1,22 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab4.DataUnits.DataUnitsVisitors;
+
+namespace Itmo.ObjectOrientedProgramming.Lab4.DataUnits.Directories;
+
+public abstract class Directory : IDataUnit
+{
+    public abstract Uri Uri { get; }
+
+    public abstract IList<Directory> SubDirectories { get; init; }
+
+    public abstract IList<Files.File> Files { get; init; }
+
+    public void Accept(IDataUnitVisitor visitor)
+    {
+        visitor.VisitDirectory(this);
+    }
+
+    public int CompareTo(IDataUnit? other)
+    {
+        return other != null ? Uri.Compare(Uri, other.Uri, UriComponents.AbsoluteUri, UriFormat.Unescaped, StringComparison.CurrentCulture) : 0;
+    }
+}
